7556a10b50efcf0437ae044132da3222bfb567a3
UMUTONIRitha <rithamarie9@gmail.com>
2021-02-09 17:03:45 +0200

    add changes

--- art_app/admin.py
+++ art_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from.models import Art, Profile, Comments,Category
 # Register your models here.
+
+admin.site.register(Art)
+admin.site.register(Profile)
+admin.site.register(Comments)
+admin.site.register(Category)
